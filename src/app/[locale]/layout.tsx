b510266511565d229d7bdb50e82e146c8524f8f6
yidongw <wy.dong96@gmail.com>
2025-04-13 17:57:09 +0800

    Initial commit

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,76 @@
+import type { Metadata } from 'next';
+import { PostHogProvider } from '@/components/analytics/PostHogProvider';
+import { DemoBadge } from '@/components/DemoBadge';
+import { routing } from '@/libs/i18nNavigation';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages, setRequestLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { Providers } from './providers';
+import '@/styles/global.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      url: '/favicon.ico',
+    },
+  ],
+};
+
+export function generateStaticParams() {
+  return routing.locales.map(locale => ({ locale }));
+}
+
+export default async function RootLayout(props: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await props.params;
+
+  if (!routing.locales.includes(locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  // Using internationalization in Client Components
+  const messages = await getMessages();
+
+  // The `suppressHydrationWarning` attribute in <body> is used to prevent hydration errors caused by Sentry Overlay,
+  // which dynamically adds a `style` attribute to the body tag.
+
+  return (
+    <html lang={locale}>
+      <body suppressHydrationWarning>
+        <Providers>
+          <NextIntlClientProvider
+            locale={locale}
+            messages={messages}
+          >
+            <PostHogProvider>
+              {props.children}
+            </PostHogProvider>
+            <DemoBadge />
+          </NextIntlClientProvider>
+        </Providers>
+      </body>
+    </html>
+  );
+}
